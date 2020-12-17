69875adefe05a58faa82987066244547b03a6ea8
Haralan Dobrev <harry@clippings.com>
2020-12-17 22:05:00 +0200

    Disable compression plugin in webpack

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const baseConfig = require('./base.config.js');
-const CompressionPlugin = require('compression-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
 //const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(baseConfig, {
@@ -17,12 +17,12 @@ module.exports = merge(baseConfig, {
         //new UglifyJsPlugin({sourceMap: false, uglifyOptions: {compress: true}}),
         new webpack.LoaderOptionsPlugin({minimize: true}),
         new webpack.optimize.AggressiveMergingPlugin(),
-        new CompressionPlugin({
-            filename: "[path].gz[query]",
-            algorithm: "gzip",
-            test: /\.js$|\.css$|\.html$/,
-            //threshold: 10240,,
-            minRatio: 0.8,
-        })
+        // new CompressionPlugin({
+        //     filename: "[path].gz[query]",
+        //     algorithm: "gzip",
+        //     test: /\.js$|\.css$|\.html$/,
+        //     //threshold: 10240,,
+        //     minRatio: 0.8,
+        // })
     ]
 });
