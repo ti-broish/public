a5eb63ad52b259413c5c28dbd26b0658139db992
Blagoslav Mihaylov <blago@bluedottech.io>
2021-01-14 16:36:13 +0200

    Added form submit conversion tracking

--- src/components/front/pages/SignUp.js
+++ src/components/front/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 import Helmet from 'react-helmet';
@@ -15,24 +15,38 @@ const FormWrapper = styled.div`
 `;
 
 export default () => {
-    const [loadCount, setLoadCount] = useState(0);
-
-    // Second iframe load is after a submission
-    // Do not count future loads after editing a response as submissions
-    if (loadCount === 2) {
-        window.gtag && gtag('event', 'conversion', {
-            'send_to': 'AW-859816919/zYXnCPLNwOgBENeH_5kD',
-        });
-        axios.get('https://dabulgaria.bg/zastapnik.php');
-    }
 
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, []);
 
-    const onSignupIframeLoad = () => {
-        setLoadCount(loadCount + 1);
-    };
+        var eventMethod = window.addEventListener
+			? "addEventListener"
+            : "attachEvent";
+        
+        var eventer = window[eventMethod];
+        var messageEvent = eventMethod === "attachEvent"
+            ? "onmessage"
+            : "message";
+
+        const formSubmitHandler = e => {
+            if (e.data === "formSubmitted" || e.message === "formSubmitted") {
+                window.gtag && gtag('event', 'conversion', {
+                    'send_to': 'AW-859816919/zYXnCPLNwOgBENeH_5kD',
+                });
+                axios.get('https://dabulgaria.bg/zastapnik.php');
+            }
+        };
+
+        eventer(messageEvent, formSubmitHandler);
+
+        return () => {
+            var cleanupMethod = window.addEventListener
+                ? "removeEventListener"
+                : "detachEvent";
+            var cleanup = window[cleanupMethod];
+            cleanup(messageEvent, formSubmitHandler);
+        };
+    }, []);
 
     let metaTitle = "Запиши се още сега | Ти Броиш";
     let metaUrl = "https://tibroish.bg/signup/";
@@ -69,7 +83,7 @@ export default () => {
                     Запишете се днес и до няколко дни ще се свържем с Вас, за да Ви въведем в кампанията. 
                 </p>
                 <FormWrapper>
-                    <iframe id="gform" style={{border: 'none'}} sandbox="allow-same-origin" onLoad={onSignupIframeLoad} src="https://dabulgaria.bg/stani-zastapnik-embed/">Loading...</iframe>
+                    <iframe id="gform" style={{border: 'none'}} sandbox="allow-same-origin" src="https://dabulgaria.bg/stani-zastapnik-embed/">Loading...</iframe>
                     {/*<iframe id="gform" onLoad={onSignupIframeLoad} src="https://docs.google.com/forms/d/e/1FAIpQLSdXMU-qZsIDLMEymzZl7VMthQyC0gJ0-X0Ew8wZo8P3oLHElg/viewform?embedded=true" frameBorder="0">Loading…</iframe>*/}
                 </FormWrapper>
             </MainContent>
