c5218fb1ddf5dc526bc33cf5af799ae215577577
svilenzhelev <zhelev@gmail.com>
2023-03-27 22:31:20 +0300

    Update Instructions.js

--- src/components/front/pages/Instructions.js
+++ src/components/front/pages/Instructions.js
@@ -6,15 +6,13 @@ import MobileAppInstructions from './instructions/MobileAppInstructions';
 import WatcherInstructions from './instructions/WatcherInstructions';
 import WhatToWatchForInstructions from './instructions/WhatToWatchForInstructions';
 import InstructionsIndex from './instructions/InstructionsIndex';
-import HowToTakePhotos from './instructions/HowToTakePhotos';
 
 export default props => {
     return(
         <Switch>
+            <Route path='/instructions/what-to-watch-for' component={WhatToWatchForInstructions}/>
             <Route path='/instructions/mobile-app' component={MobileAppInstructions}/>
             <Route path='/instructions/watchers' component={WatcherInstructions}/>
-            <Route path='/instructions/what-to-watch-for' component={WhatToWatchForInstructions}/>
-            <Route path='/instructions/how-to-take-photos' component={HowToTakePhotos}/>
             <Route exact path='/instructions' component={InstructionsIndex}/>
             <Redirect to='/instructions'/>
         </Switch>
