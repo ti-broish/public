c7cabbe788c7e2c9e97a315e05a96260d0f253a6
svilenzhelev <zhelev@gmail.com>
2023-03-30 22:27:40 +0300

    Update Instructions.js

--- src/components/front/pages/Instructions.js
+++ src/components/front/pages/Instructions.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import MobileAppInstructions from './instructions/MobileAppInstructions';
+import HowToValidate from './instructions/HowToValidate';
 import WatcherInstructions from './instructions/WatcherInstructions';
 import WhatToWatchForInstructions from './instructions/WhatToWatchForInstructions';
 import InstructionsIndex from './instructions/InstructionsIndex';
@@ -12,6 +13,7 @@ export default props => {
         <Switch>
             <Route path='/instructions/what-to-watch-for' component={WhatToWatchForInstructions}/>
             <Route path='/instructions/mobile-app' component={MobileAppInstructions}/>
+            <Route path='/instructions/how-to-validate' component={HowToValidate}/>
             <Route path='/instructions/watchers' component={WatcherInstructions}/>
             <Route exact path='/instructions' component={InstructionsIndex}/>
             <Redirect to='/instructions'/>
