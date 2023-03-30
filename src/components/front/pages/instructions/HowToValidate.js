288841fc80d58bb1210bf969027b7495cfd2fc60
svilenzhelev <zhelev@gmail.com>
2023-03-30 23:03:10 +0300

    Update HowToValidate.js

--- src/components/front/pages/instructions/HowToValidate.js
+++ src/components/front/pages/instructions/HowToValidate.js
@@ -64,7 +64,7 @@ export default (props) => {
         <meta property="og:image:height" content={'628'} />
       </Helmet>
       <MainContent>
-        <h1>Инструкции за валидиране на протокол в “Ти броиш”</h1>
+        <h1>Инструкции за валидиране на протокол в “Ти&nbsp;броиш”</h1>
         <hr />
         <InstructionAnchor href="/files/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F-%D0%B7%D0%B0-%D0%B2%D0%B0%D0%BB%D0%B8%D0%B4%D0%B8%D1%80%D0%B0%D0%BD%D0%B5-%D0%A2%D0%B8-%D0%B1%D1%80%D0%BE%D0%B8%D1%88.pdf">
           Как се валидира протокол
