9c45b7a11e12d5d94679bc4add8c1ac1e98a78a1
svilenzhelev <zhelev@gmail.com>
2023-03-30 22:48:44 +0300

    Update HowToValidate.js

--- src/components/front/pages/instructions/HowToValidate.js
+++ src/components/front/pages/instructions/HowToValidate.js
@@ -65,7 +65,7 @@ export default (props) => {
       <MainContent>
         <h1>Инструкции за валидиране на протокол в “Ти броиш”</h1>
         <hr />
-        <InstructionAnchor href="/files/Инструкция-за-валидиране-Ти-броиш.pdf">
+        <InstructionAnchor href="/files/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F-%D0%B7%D0%B0-%D0%B2%D0%B0%D0%BB%D0%B8%D0%B4%D0%B8%D1%80%D0%B0%D0%BD%D0%B5-%D0%A2%D0%B8-%D0%B1%D1%80%D0%BE%D0%B8%D1%88.pdf">
           Как се валидира протокол
         </InstructionAnchor>
         <YouTubeVideoEmbed videoId={'j2ChYutSk-c'} />
