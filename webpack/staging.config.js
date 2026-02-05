474f2e0fce05e2a8d8a10a69787ce1d6e3b5bd2e
Haralan Dobrev <harry@hkdobrev.com>
2026-02-06 01:46:59 +0200

    Remove uglify usage

--- webpack/staging.config.js
+++ webpack/staging.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 //const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const baseConfig = require('./base.config.js');
 // const CompressionPlugin = require('compression-webpack-plugin');
 //const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
